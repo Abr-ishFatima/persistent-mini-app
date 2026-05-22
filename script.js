84718300537c7f4b02ef4ea268cfb1ed4b5fe8f1
Abr-ishFatima <fatimaabrish.fa@gmail.com>
2026-05-22 14:02:21 +0500

    added persistent feature using saveData() fnx

--- script.js
+++ script.js
@@ -13,5 +13,21 @@ function addTask(){
         span.innerHTML = '\u00d7';
         li.appendChild(span);
     }
-    inputBox.value='';
+    inputBox.value="";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
 }
